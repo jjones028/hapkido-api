9072f8366ae272fb7bf41f104169aa51299fe773
Jeremy Jones <jjones028@gmail.com>
2024-02-22 19:08:13 -0500

    Initial project commit.

---
+++ src/main/kotlin/io/spektacle/Application.kt
@@ -0,0 +1,14 @@
+package io.spektacle
+
+import io.ktor.server.application.*
+import io.spektacle.plugins.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureDatabases()
+    configureRouting()
+}
