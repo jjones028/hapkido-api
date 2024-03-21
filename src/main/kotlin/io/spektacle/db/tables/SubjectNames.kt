e8e2b9f0e7c94ea2760f382f438a3ed7f125bc3b
Jeremy Jones <jjones028@gmail.com>
2024-03-20 20:44:11 -0400

    Add SubjectName Table/Repository/Route

---
+++ src/main/kotlin/io/spektacle/db/tables/SubjectNames.kt
@@ -0,0 +1,17 @@
+package io.spektacle.db.tables
+
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.Table
+
+@Suppress("MagicNumber")
+object SubjectNames : Table() {
+    val id: Column<Long> = long("id").autoIncrement()
+    val commonName: Column<String> = varchar("common_name", 100).uniqueIndex()
+    val countryCode: Column<String> = varchar("country_code", 3)
+    val organization: Column<String> = varchar("organization", 250)
+    val stateOrProvidence: Column<String?> = varchar("state_or_providence", 100).nullable()
+    val organizationalUnit: Column<String?> = varchar("organizational_unit", 100).nullable()
+    val locality: Column<String?> = varchar("locality", 100).nullable()
+    val emailAddress: Column<String?> = varchar("email_address", 100).nullable()
+    override val primaryKey = PrimaryKey(id, name = "pk_subject_names_id")
+}
--- src/main/kotlin/io/spektacle/plugins/Routing.kt
+++ src/main/kotlin/io/spektacle/plugins/Routing.kt
@@ -1,11 +1,13 @@
 package io.spektacle.plugins
 
-import io.ktor.server.application.*
-import io.ktor.server.routing.*
+import io.ktor.server.application.Application
+import io.ktor.server.routing.routing
+import io.spektacle.routes.subjectNameRoutes
 import io.spektacle.routes.userRoutes
 
 fun Application.configureRouting() {
     routing {
         userRoutes()
+        subjectNameRoutes()
     }
 }
